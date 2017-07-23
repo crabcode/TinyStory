9022c5b42c809f5eb43e7868ffb00fa3b02803d0
crabcode <crabman@hypnoticowl.com>
2017-07-23 17:11:58 +0200

    Initial Commit

---
+++ README.md
@@ -0,0 +1,109 @@
+TinyScript
+----------
+
+TinyScript is small indentation-based Choose Your Own Adventure engine I wrote because why not.
+
+Syntax
+------
+
+The most important thing to note is that the nesting of text and options is achieved with indentation. For that, you must either use tabs or 4 spaces, but mustn't mix the two.
+
+Otherwise, the easiest way to see how it works would be to check out the sample.txt, but here's a list of commands you can currently use:
+
+#### Options
+```
+* I am a clickable option
+    And this is the text that will be displayed if you click it
+```
+
+#### Variables
+```
+= variable value
+```
+
+#### Conditions
+```
+: variable true
+    * I appear only if the variable is true
+: variable false
+    * And I appear if it is false
+```
+
+#### Jumps
+Jumps simply use the text of the option you want to jump to. Obviously that's not a great idea because it will get confused if multiple options in your game have the same text. I'll probably change that to a proper labeling system at some point.
+
+```
+* Option text
+    We're going to jump here in a second
+* Here we go
+    > Option text
+```
+
+Note: Putting `> End` will end the game there.
+
+#### Links
+Links work pretty much the same away, except they don't erase the displayed text, so you can merge texts.
+
+```
+* I am an option!
+    Things are happening here!
+* And now for a jump...
+    As we can see:
+    < I am an option!
+```
+
+Output:
+>As we can see:
+>Things are happening here!
+
+Customization
+-------------
+
+#### Filename
+If you would like to change the name of the story file, tell TinyScript what it is in the index.html by setting
+```
+TinyScript.filename = "yourname.txt"
+```
+prior to load().
+
+#### Title
+This will change the HTML title and add the title to the top of page as well:
+```
+# title A Cool Title
+```
+
+#### Font
+You can easily add a Google Font by adding:
+```
+# font Google Font Name
+```
+
+Example
+-------
+Finally, here's a quick example to make all of that up there a little clearer:
+```
+# title Example
+
+What would you like to do?
+
+* I want to set a variable.
+    Alrighty, let's do it!
+    * Set it to true
+        Done.
+        = var true
+    * Set it to 5
+        Okay.
+        = var 5
+        // Note that if no further options or jumps exist, the previous options are displayed again
+    * Now I'd like to do something else.
+        > I want to do something else.
+    * Okay, that's enough.
+        As you wish.
+        > End
+* I want to do something else.
+    : var undefined
+        I see you didn't set the variable.
+        > End
+```
+
+Check out the `sample.txt` for a more complete example.
---
+++ system/tinystory.js
@@ -0,0 +1,258 @@
+window.TinyStory =
+{
+	filename: "story.txt",
+    fadeSpeed: 200,
+	data: [],
+	index: [],
+	vars: {},
+	space: undefined,
+	
+	load: function load()
+	{
+		$.ajax({
+            url: this.filename,
+            dataType: "text",
+            success: function(data) {
+                TinyStory.parse(data);
+            },
+            error: function(e) {
+                $("#error").text("'" + TinyStory.filename + "' not found. Make sure the file is at the specified path.");
+            }
+        });
+	},
+	
+	parse: function parse(data)
+	{
+		this.raw = data.split(/\r?\n/);
+		
+		var lastObj = [];
+		
+		for (var i = 0; i < this.raw.length; i++)
+		{
+			var obj = {};
+			
+			var line = this.raw[i].trim();
+            
+            if (line.substr(0,2) == "//")
+                continue;
+			
+			if (line.length == 0)
+				continue;
+			
+			obj.indent = this.raw[i].search(/\S/);
+			
+			if (this.space === undefined && obj.indent > 0)
+				this.space = (this.raw[i][0] == " ");
+			
+			if (this.space)
+				obj.indent /= 4;
+			
+			if (obj.indent != Math.round(obj.indent))
+			{
+				$("#error").html("Faulty indentation detected on line " + (i+1) + ".<br/>Indentation must be either tab or 4 spaces and can't be mixed.");
+				return;
+			}
+			
+			switch (line[0])
+			{
+				case "*":
+					obj.content = line.substr(1).trim();
+					obj.type = "Option";
+					this.index[obj.content] = obj;
+					break;
+					
+				case ":":
+					obj.content = line.substr(1).trim();
+					obj.type = "Condition";
+					break;
+					
+				case "=":
+					obj.content = line.substr(1).trim();
+					obj.type = "Operation";
+					break;
+					
+				case ">":
+					obj.content = line.substr(1).trim();
+					obj.type = "Jump";
+					break;
+					
+				case "<":
+					obj.content = line.substr(1).trim();
+					obj.type = "Link";
+					break;
+				
+				case "#":
+					var cmd = line.substr(1).trim();
+					var value = cmd.substr(cmd.indexOf(" ")+1).trim();
+                    
+                    console.log(value);
+                    
+					switch (cmd.substr(0, cmd.indexOf(" ")))
+					{
+						case "title":
+							$("title").text(value);
+                            $("body").prepend($(document.createElement("div"))
+                                             .attr("id", "title")
+                                             .text(value));
+							break;
+                        
+                        case "font":
+                            $("head").append($(document.createElement("link"))
+                                                .attr("href", "https://fonts.googleapis.com/css?family=" + value.replace(" ", "+"))
+                                                .attr("rel", "stylesheet"));
+                            $("html").css("font-family", "'" + value + "', sans-serif")
+                            break;
+						
+						default:
+							break;
+					}
+					
+					continue;
+					break;
+				
+				default:
+					obj.content = line;
+					obj.type = "Text";
+					break;
+			}
+			
+			obj.children = [];
+			
+			if (obj.indent == 0)
+			{
+				this.data.push(obj);
+				lastObj[0] = obj;
+			}
+			else
+			{
+				lastObj[obj.indent-1].children.push(obj);
+                obj.parent = lastObj[obj.indent-1];
+				lastObj[obj.indent] = obj;
+			}
+		}
+		
+		this.run();
+	},
+	
+	run: function run()
+	{
+		$("#error").remove();
+		
+		var wrapper = $(document.createElement("div"))
+                        .attr("id", "wrapper")
+                        .css("opacity", 0);
+		$("body").append(wrapper);
+		
+		this.loadNode(this.data);
+	},
+	
+	loadNode: function loadNode(data, keep, optionsOnly)
+	{
+		if (!keep)
+			$("#wrapper").empty();
+		
+        var deadend = true;
+        
+		for (var i = 0; i < data.length; i++)
+		{
+			var obj = data[i];
+			
+			switch(obj.type)
+			{
+				case "Option":
+					$("#wrapper").append($(document.createElement("div"))
+										.addClass("option")
+									    .text(obj.content)
+									    .click(this.delayedLoadNode.bind(this, obj.children, false, false)));
+                    
+                    deadend = false;
+					break;
+					
+				case "Text":
+                    if (optionsOnly)
+                        break;
+                    
+					$("#wrapper").append($(document.createElement("div"))
+										.addClass("text")
+									    .text(obj.content));
+					break;
+					
+				case "Condition":
+					var variable = obj.content.substr(0, obj.content.indexOf(" "));
+					var value = obj.content.substr(obj.content.indexOf(" ")+1);
+					
+					switch (value)
+					{
+						case "undefined":
+							value = undefined;
+							break;
+						case "null":
+							value = null;
+						case "NaN":
+							value = NaN;
+					}
+					
+					if (this.vars[variable] == value)
+						this.delayedLoadNode(obj.children, true);
+					break;
+					
+				case "Operation":
+					var variable = obj.content.substr(0, obj.content.indexOf(" "));
+					var value = obj.content.substr(obj.content.indexOf(" ")+1);
+					
+					switch (value)
+					{
+						case "undefined":
+							value = undefined;
+							break;
+						case "null":
+							value = null;
+						case "NaN":
+							value = NaN;
+					}
+					
+					this.vars[variable] = value;
+					break;
+					
+				case "Jump":
+                    deadend = false;
+                    
+					if (obj.content.toLowerCase() == "end")
+                    {
+						break;
+                    }
+					else if (this.index[obj.content] == undefined)
+						console.error("Index '" + obj.content + "' not found");
+					else
+						this.delayedLoadNode(this.index[obj.content].children, false, false);
+					break;
+					
+				case "Link":
+                    deadend = false;
+                    
+					if (obj.content.toLowerCase() == "end")
+						break;
+					else if (this.index[obj.content] == undefined)
+						console.error("Index '" + obj.content + "' not found");
+					else
+						this.delayedLoadNode(this.index[obj.content].children, true, false);
+					break;
+			}
+		}
+            
+        if (deadend)
+            this.delayedLoadNode(obj.parent.parent.type == "Condition" ? obj.parent.parent.parent.children : obj.parent.parent.children, true, true);
+        
+        $("#wrapper").animate({ opacity: 1 }, this.fadeSpeed);
+	},
+    
+    delayedLoadNode: function(data, keep, optionsOnly)
+    {
+        if ($("#wrapper").css("opacity") > 0)
+            $("#wrapper").animate({ opacity: 0 }, this.fadeSpeed, function() {
+                this.loadNode(data, keep, optionsOnly);
+            }.bind(this));
+        else
+            this.loadNode(data, keep, optionsOnly);
+    }
+}
